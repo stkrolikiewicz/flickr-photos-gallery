ffeab1de5d77aacd3a49051799328f4ff1493113
stachkrol <st.krolikiewicz@gmail.com>
2023-02-21 13:13:34 +0100

    Update tailwind.config.js
    In Tailwind v3. 'purge' is replaced with 'content'.
    You don't have to use 'darkMode: false' and
    'variants' also should be deleted.
    Check out tailwind v3 upgrade guide:
    https://tailwindcss.com/docs/upgrade-guide

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: "jit",
-  purge: [
+  content: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
