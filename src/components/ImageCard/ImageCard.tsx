3c8e27e9160d873a66c93b3eac0445a1f7c00762
stachkrol <st.krolikiewicz@gmail.com>
2023-03-08 12:28:25 +0100

    Add image lazy loading animated plaecholder

--- src/components/ImageCard/ImageCard.tsx
+++ src/components/ImageCard/ImageCard.tsx
@@ -2,6 +2,7 @@ import { PhotoInfo } from "~/types"
 import Image from "next/image"
 import styles from "./ImageCard.module.css"
 import { UserCircleIcon, CalendarIcon } from '@heroicons/react/24/solid'
+import { blurDataURL } from "~/utils/blurDataURL"
 
 interface Props {
   photo: PhotoInfo
@@ -11,11 +12,14 @@ const ImageCard = ({ photo }: Props) => {
   return (
     <div className={styles.container}>
       <Image
-        className={styles.image}
+        placeholder='blur'
+        blurDataURL={blurDataURL}
+        className={`${styles.image} animate-pulse`}
         src={`https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`}
         width={500}
         height={500}
         alt={photo.title}
+        onLoadingComplete={(e) => console.log(e.classList.remove('animate-pulse'))}
       />
       <div className={styles.content}>
         <h4 className={styles.title}>{photo.title}</h4>
