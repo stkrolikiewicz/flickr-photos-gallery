9b3e0e664de13f791cd6ae2577cd1cdb7d022352
stachkrol <st.krolikiewicz@gmail.com>
2023-03-08 12:32:36 +0100

    Create and implement Loader component
    Additionally button to load more dataa has been
    updated

--- src/components/PhotosList/PhotosList.tsx
+++ src/components/PhotosList/PhotosList.tsx
@@ -1,21 +1,28 @@
+import { SetStateAction, useEffect } from "react"
 import usePhotos from "~/hooks/usePhotos"
+import Loader from "../Loader/Loader"
 import PhotosRow from "../PhotosRow/PhotosRow"
 
 interface Props {
   index: number,
+  setIsLoading?: React.Dispatch<SetStateAction<boolean>>
 }
 
-const PhotosList = ({ index }: Props) => {
+const PhotosList = ({ index, setIsLoading }: Props) => {
   const { data, isLoading, isValidating, error } = usePhotos(index)
 
   const photos = isLoading ? null : data?.photo
   const oddRow = photos?.slice(0, 50)
   const evenRow = photos?.slice(50, 100)
 
+  useEffect(() => {
+    setIsLoading && setIsLoading(isLoading)
+  }, [isLoading, setIsLoading])
+
   return (
     <>
       {
-        isLoading || isValidating ? <h2>Loading...</h2> : oddRow && evenRow && <>
+        isLoading || isValidating ? <Loader /> : oddRow && evenRow && <>
           <PhotosRow photos={oddRow} odd />
           <PhotosRow photos={evenRow} />
         </>
