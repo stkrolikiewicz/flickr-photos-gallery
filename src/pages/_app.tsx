46cff7bf81f7e67c4849daca162640e6b01a342b
stachkrol <st.krolikiewicz@gmail.com>
2023-03-10 17:36:43 +0100

    Display only one photos list on the home page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,17 +7,4 @@ export default function App({ Component, pageProps, router }: AppProps) {
   return <PagesCountProvider>
     <Component {...pageProps} />
   </PagesCountProvider>
-  // return <motion.div key={router.route} initial="pageInitial" animate="pageAnimate" exit="pageExit" variants={{
-  //   pageInitial: {
-  //     opacity: 0
-  //   },
-  //   pageAnimate: {
-  //     opacity: 1
-  //   },
-  //   pageExit: {
-  //     opacity: 0
-  //   }
-  // }}>
-
-  // </motion.div>
 }
