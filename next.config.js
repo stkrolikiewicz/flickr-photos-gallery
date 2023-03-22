388c010dae0c2ba9068365ccfc708d35c26154e0
stkrolikiewicz <46401055+stkrolikiewicz@users.noreply.github.com>
2023-03-22 18:45:06 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  /* images: {
-    formats: ["image/avif", "image/webp"],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "live.staticflickr.com",
-        port: "",
-        pathname: "/**/**",
-      },
-      unoptimized: "true",
-    ],
-  }, */
   env: {
     API_KEY: '06fe421ea76dcc08939e8e4202b4d933',
   },
