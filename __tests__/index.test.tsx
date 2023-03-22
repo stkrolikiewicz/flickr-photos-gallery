8b4edd6a73922ecdd1733eadfbe6c59aa6de6ce7
stachkrol <st.krolikiewicz@gmail.com>
2023-03-22 14:04:45 +0100

    Fix bug connected with mocked router

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -2,6 +2,8 @@ import { render, screen } from '@testing-library/react'
 import Home from '~/pages/index'
 import '@testing-library/jest-dom'
 
+jest.mock('next/router', () => require('next-router-mock'));
+
 describe('Home', () => {
   it('renders a logo heading', () => {
     render(<Home />)
